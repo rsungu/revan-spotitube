9c832298ef77fcd17bbeee48d73f2312befc5aba
rsungu <38646349+rsungu@users.noreply.github.com>
2019-03-19 09:48:50 +0100

    19-3-2019

--- src/test/java/nl/han/oose/LoginControllerTest.java
+++ src/test/java/nl/han/oose/LoginControllerTest.java
@@ -1,5 +1,45 @@
-import static org.junit.Assert.*;
+package nl.han.oose;
+
+import nl.han.oose.controller.LoginController;
+import nl.han.oose.dto.ErrorDTO;
+import nl.han.oose.dto.TokenDTO;
+import nl.han.oose.dto.UserDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import javax.ws.rs.core.Response;
+
 
 public class LoginControllerTest {
+
+    private LoginController sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new LoginController();
+    }
+
+    @Test
+    void loginSucces() {
+        UserDTO userDTO = new UserDTO("revan", "1234");
+        Response actualResult = sut.login(userDTO);
+        assertEquals(Response.Status.OK.getStatusCode(), actualResult.getStatus());
+
+        TokenDTO tokenDTO = (TokenDTO) actualResult.getEntity();
+        assertEquals("12345", tokenDTO.getToken());
+        assertEquals("Revan Sungu", tokenDTO.getUser());
+    }
+
+    @Test
+    void loginFailure() {
+        UserDTO userDTO = new UserDTO("revan", "12345");
+        Response actualResult = sut.login(userDTO);
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), actualResult.getStatus());
+
+        ErrorDTO errorDTO = (ErrorDTO) actualResult.getEntity();
+        assertEquals("Login failed for user: " + userDTO.getUsername(), errorDTO.getMessage());
+
+    }
 
 }
